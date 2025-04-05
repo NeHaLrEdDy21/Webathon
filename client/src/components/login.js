e974f029e06f6c6142aa78fc1b1598699f394ffa
Nehal Reddy <mudiam.nehal@gmail.com>
2025-04-05 16:05:58 +0530

    dasauto

---
+++ client/src/components/login.js
@@ -0,0 +1,49 @@
+// src/pages/Login.js
+import React, { useState } from 'react';
+import '../styles/auth.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:4000/api/auth/login', {email, password });
+      alert('Login successful!');
+      localStorage.setItem('token', res.data.token);
+    } catch (err) {
+      alert(err.response?.data?.message || 'Login failed');
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        <p>
+          Don’t have an account? <Link to="/register">Register</Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
